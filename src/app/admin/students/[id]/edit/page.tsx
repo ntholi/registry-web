f48e80ff26534bd4f2bd315edf77c73200e7100c
Ntholi Nkhatho <ntholi@live.com>
2025-01-28 19:35:53 +0200

    correct issue with placing page.tsx in the wrong directory

--- src/app/admin/students/[id]/edit/page.tsx
+++ src/app/admin/students/[id]/edit/page.tsx
@@ -1,18 +1,29 @@
-import { getStudent } from '@/server/students/actions';
+import { Box } from '@mantine/core';
 import { notFound } from 'next/navigation';
-import StudentView from './StudentView';
+import Form from '../../Form';
+import { getStudent, updateStudent } from '@/server/students/actions';
 
 type Props = {
   params: Promise<{ id: string }>;
 };
 
-export default async function StudentDetails({ params }: Props) {
+export default async function StudentEdit({ params }: Props) {
   const { id } = await params;
   const student = await getStudent(Number(id));
-
   if (!student) {
     return notFound();
   }
 
-  return <StudentView student={student} />;
+  return (
+    <Box p={'lg'}>
+      <Form
+        title={'Edit Student'}
+        defaultValues={student}
+        onSubmit={async (value) => {
+          'use server';
+          return await updateStudent(Number(id), value);
+        }}
+      />
+    </Box>
+  );
 }
--- src/app/admin/students/[id]/page.tsx
+++ src/app/admin/students/[id]/page.tsx
@@ -1,11 +1,6 @@
-import {
-  DetailsView,
-  DetailsViewHeader,
-  FieldView,
-  DetailsViewBody,
-} from '@/components/adease';
+import { getStudent } from '@/server/students/actions';
 import { notFound } from 'next/navigation';
-import { getStudent, deleteStudent } from '@/server/students/actions';
+import StudentView from './StudentView';
 
 type Props = {
   params: Promise<{ id: string }>;
@@ -19,31 +14,5 @@ export default async function StudentDetails({ params }: Props) {
     return notFound();
   }
 
-  return (
-    <DetailsView>
-      <DetailsViewHeader
-        title={'Student'}
-        queryKey={['students']}
-        handleDelete={async () => {
-          'use server';
-          await deleteStudent(Number(id));
-        }}
-      />
-      <DetailsViewBody>
-        <FieldView label='Student No'>{student.stdNo}</FieldView>
-        <FieldView label='National Id'>{student.nationalId}</FieldView>
-        <FieldView label='Name'>{student.name}</FieldView>
-        <FieldView label='Phone 1'>{student.phone1}</FieldView>
-        <FieldView label='Phone 2'>{student.phone2}</FieldView>
-        <FieldView label='Religion'>{student.religion}</FieldView>
-        <FieldView label='Date of Birth'>
-          {new Date(student.dateOfBirth).toLocaleDateString()}
-        </FieldView>
-        <FieldView label='Gender'>{student.gender}</FieldView>
-        <FieldView label='Marital Status'>{student.maritalStatus}</FieldView>
-        <FieldView label='Structure ID'>{student.structureId}</FieldView>
-        <FieldView label='User ID'>{student.userId}</FieldView>
-      </DetailsViewBody>
-    </DetailsView>
-  );
+  return <StudentView student={student} />;
 }
