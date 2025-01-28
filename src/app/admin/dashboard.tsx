5451ddc0653fccb9da66e316b452fca159df2c83
Ntholi Nkhatho <ntholi@live.com>
2025-01-28 19:12:42 +0200

    re-add Logo component

--- src/app/admin/dashboard.tsx
+++ src/app/admin/dashboard.tsx
@@ -1,12 +1,27 @@
 'use client';
 
 import { Shell } from '@/components/adease';
-import { ActionIcon, Avatar, Flex, Group, Stack, Text, LoadingOverlay, Image } from '@mantine/core';
+import {
+  ActionIcon,
+  Avatar,
+  Flex,
+  Group,
+  Stack,
+  Text,
+  LoadingOverlay,
+  Image,
+  useComputedColorScheme,
+} from '@mantine/core';
 import { modals } from '@mantine/modals';
 import { signOut, useSession } from 'next-auth/react';
 import { useRouter } from 'next/navigation';
 import React from 'react';
-import { Icon, IconChevronRight, IconLogout2, IconUsers } from '@tabler/icons-react';
+import {
+  Icon,
+  IconChevronRight,
+  IconLogout2,
+  IconUsers,
+} from '@tabler/icons-react';
 import { usePathname } from 'next/navigation';
 import { Indicator, NavLink } from '@mantine/core';
 import Link from 'next/link';
@@ -33,11 +48,7 @@ const navigation: NavItem[] = [
   },
 ];
 
-export default function Dashboard({
-  children,
-}: {
-  children: React.ReactNode;
-}) {
+export default function Dashboard({ children }: { children: React.ReactNode }) {
   const { status } = useSession();
 
   if (status === 'loading') {
@@ -52,7 +63,7 @@ export default function Dashboard({
     <Shell>
       <Shell.Header>
         <Group>
-          <Image src={'/images/logo.png'} height={40} alt='logo' />
+          <Logo />
         </Group>
       </Shell.Header>
       <Shell.Navigation>
@@ -144,7 +155,9 @@ function ItemDisplay({ item }: { item: NavItem }) {
       href={item.href || ''}
       active={item.href ? pathname.startsWith(item.href) : false}
       leftSection={<Icon size='1.1rem' />}
-      rightSection={item.href ? <IconChevronRight size='0.8rem' stroke={1.5} /> : undefined}
+      rightSection={
+        item.href ? <IconChevronRight size='0.8rem' stroke={1.5} /> : undefined
+      }
       opened={!!item.children}
     >
       {item.children?.map((child, index) => (
@@ -153,4 +166,16 @@ function ItemDisplay({ item }: { item: NavItem }) {
     </NavLink>
   );
   return navLink;
+}
+
+function Logo() {
+  const colorScheme = useComputedColorScheme('dark');
+  return (
+    <Image
+      src={`/images/logo-${colorScheme}.png`}
+      w={'auto'}
+      h={50}
+      alt='logo'
+    />
+  );
 }
