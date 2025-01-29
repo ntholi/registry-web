b81593549ce7361e3b91584f76e7c59ad9d135b6
Ntholi Nkhatho <ntholi@live.com>
2025-01-29 16:06:28 +0200

    Update User role type in next-auth module to use UserRole from schema

--- next-auth.ts
+++ next-auth.ts
@@ -1,9 +1,8 @@
-import { users } from '@/db/schema';
+import { UserRole } from '@/db/schema';
 import { User as DefaultUser } from 'next-auth';
 
-type Role = (typeof users.$inferSelect)['role'];
 declare module 'next-auth' {
   interface User extends DefaultUser {
-    role: Role;
+    role: UserRole;
   }
 }
