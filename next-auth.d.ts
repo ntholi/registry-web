7ad0ab5c05a54c6ceb92e38151012968164c504b
Ntholi Nkhatho <ntholi@live.com>
2025-01-30 12:43:21 +0200

    add stdNo to user session

--- next-auth.d.ts
+++ next-auth.d.ts
@@ -4,5 +4,6 @@ import { User as DefaultUser } from 'next-auth';
 declare module 'next-auth' {
   interface User extends DefaultUser {
     role: UserRole;
+    stdNo?: number;
   }
 }
--- src/auth.ts
+++ src/auth.ts
@@ -3,7 +3,14 @@ import { DrizzleAdapter } from '@auth/drizzle-adapter';
 import NextAuth from 'next-auth';
 import type { Adapter } from 'next-auth/adapters';
 import Google from 'next-auth/providers/google';
-import { accounts, sessions, users, verificationTokens } from './db/schema';
+import {
+  accounts,
+  sessions,
+  users,
+  verificationTokens,
+  students,
+} from './db/schema';
+import { eq } from 'drizzle-orm';
 
 export const { handlers, signIn, signOut, auth } = NextAuth({
   providers: [Google],
@@ -13,9 +20,17 @@ export const { handlers, signIn, signOut, auth } = NextAuth({
     sessionsTable: sessions,
     verificationTokensTable: verificationTokens,
   }) as Adapter,
+
   callbacks: {
     async session({ session, user }) {
       session.user.role = user.role;
+
+      if (user.role === 'student') {
+        const student = await db.query.students.findFirst({
+          where: eq(students.userId, user.id),
+        });
+        session.user.stdNo = student?.stdNo;
+      }
       return session;
     },
   },
