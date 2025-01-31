91343c350d02ad0cdb9230dbad2e1bb66b755ddf
Ntholi Nkhatho <ntholi@live.com>
2025-01-31 21:23:12 +0200

    Add message field to clearanceResponses

--- src/db/schema.ts
+++ src/db/schema.ts
@@ -345,6 +345,7 @@ export const clearanceResponses = sqliteTable('clearance_responses', {
     .references(() => clearanceRequests.id, { onDelete: 'cascade' })
     .notNull(),
   department: text({ enum: dashboardUsers }).notNull(),
+  message: text(),
   clearedBy: text()
     .references(() => users.id, { onDelete: 'cascade' })
     .notNull(),
