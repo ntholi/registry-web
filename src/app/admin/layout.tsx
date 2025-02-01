2a883b66c4084c1dbe931f93389b037fe6885078
Ntholi Nkhatho <ntholi@live.com>
2025-02-01 13:06:20 +0200

    Add metadata for admin layout page

--- src/app/admin/layout.tsx
+++ src/app/admin/layout.tsx
@@ -4,9 +4,14 @@ import '@mantine/notifications/styles.css';
 
 import Dashboard from './dashboard';
 import Providers from './providers';
-import React from 'react';
+import React, { PropsWithChildren } from 'react';
+import { Metadata } from 'next';
 
-export default function AdminLayout({ children }: { children: React.ReactNode }) {
+export const metadata: Metadata = {
+  title: 'Admin | Registry Portal',
+};
+
+export default function AdminLayout({ children }: PropsWithChildren) {
   return (
     <Providers>
       <Dashboard>{children}</Dashboard>
