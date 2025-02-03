cdd562eaa14c82ba5410d1d6463c8760e8d7d484
Ntholi Nkhatho <ntholi@live.com>
2025-02-03 22:13:43 +0200

    improve ClearanceHistory

--- src/app/admin/registration-clearance/[id]/ClearanceHistory.tsx
+++ src/app/admin/registration-clearance/[id]/ClearanceHistory.tsx
@@ -1,10 +1,18 @@
 'use client';
 
-import { Accordion, Center, Loader, Stack, Table, Text } from '@mantine/core';
-import { useQuery } from '@tanstack/react-query';
-
+import { formatDateTime } from '@/lib/utils';
 import { getClearanceHistory } from '@/server/registration-clearance/actions';
-import { formatDate } from '@/lib/utils';
+import {
+  Accordion,
+  Badge,
+  Center,
+  Group,
+  Loader,
+  Paper,
+  Table,
+  Text,
+} from '@mantine/core';
+import { useQuery } from '@tanstack/react-query';
 
 type Props = {
   clearanceId: number;
@@ -18,15 +26,15 @@ export default function ClearanceHistory({ clearanceId }: Props) {
 
   if (isLoading) {
     return (
-      <Center pt={'xl'}>
-        <Loader size={'sm'} />
+      <Center pt='xl'>
+        <Loader size='md' variant='dots' />
       </Center>
     );
   }
 
   if (!history?.length) {
     return (
-      <Center pt={'xl'}>
+      <Center pt='xl'>
         <Text c='dimmed' size='sm'>
           No history found
         </Text>
@@ -35,43 +43,57 @@ export default function ClearanceHistory({ clearanceId }: Props) {
   }
 
   return (
-    <Accordion>
-      {history.map((clearance) => (
-        <Accordion.Item key={clearance.id} value={clearance.id.toString()}>
-          <Accordion.Control>
-            <Stack gap='xs'>
-              <Text fw={500}>
-                Term: {clearance.registrationRequest.term.name}
-              </Text>
-              <Text size='sm' c='dimmed'>
-                Status: {clearance.status}
-              </Text>
-            </Stack>
-          </Accordion.Control>
-          <Accordion.Panel>
-            <Table>
-              <Table.Thead>
-                <Table.Tr>
-                  <Table.Th>Date</Table.Th>
-                  <Table.Th>Status</Table.Th>
-                  <Table.Th>Message</Table.Th>
-                  <Table.Th>Updated By</Table.Th>
-                </Table.Tr>
-              </Table.Thead>
-              <Table.Tbody>
-                {clearance.audits.map((audit) => (
-                  <Table.Tr key={audit.id}>
-                    <Table.Td>{formatDate(audit.date)}</Table.Td>
-                    <Table.Td>{audit.newStatus}</Table.Td>
-                    <Table.Td>{audit.message || '-'}</Table.Td>
-                    <Table.Td>{audit.createdBy || '-'}</Table.Td>
+    <Paper shadow='sm' radius='md' pt='xl' px='xs'>
+      <Accordion variant='separated'>
+        {history.map((clearance) => (
+          <Accordion.Item key={clearance.id} value={clearance.id.toString()}>
+            <Accordion.Control>
+              <Group justify='space-between' pe={'md'}>
+                <Text fw={500}>
+                  Term: {clearance.registrationRequest.term.name}
+                </Text>
+                <Badge
+                  color={clearance.status === 'approved' ? 'green' : 'red'}
+                  variant='outline'
+                  size='sm'
+                >
+                  {clearance.status}
+                </Badge>
+              </Group>
+            </Accordion.Control>
+            <Accordion.Panel>
+              <Table striped highlightOnHover withTableBorder>
+                <Table.Thead>
+                  <Table.Tr>
+                    <Table.Th>
+                      <Group gap='xs'>Date</Group>
+                    </Table.Th>
+                    <Table.Th>
+                      <Group gap='xs'>Status</Group>
+                    </Table.Th>
+                    <Table.Th>
+                      <Group gap='xs'>Message</Group>
+                    </Table.Th>
+                    <Table.Th>
+                      <Group gap='xs'>Updated By</Group>
+                    </Table.Th>
                   </Table.Tr>
-                ))}
-              </Table.Tbody>
-            </Table>
-          </Accordion.Panel>
-        </Accordion.Item>
-      ))}
-    </Accordion>
+                </Table.Thead>
+                <Table.Tbody>
+                  {clearance.audits.map((audit) => (
+                    <Table.Tr key={audit.id}>
+                      <Table.Td>{formatDateTime(audit.date)}</Table.Td>
+                      <Table.Td>{audit.newStatus}</Table.Td>
+                      <Table.Td>{audit.message || '-'}</Table.Td>
+                      <Table.Td>{audit.createdBy}</Table.Td>
+                    </Table.Tr>
+                  ))}
+                </Table.Tbody>
+              </Table>
+            </Accordion.Panel>
+          </Accordion.Item>
+        ))}
+      </Accordion>
+    </Paper>
   );
 }
