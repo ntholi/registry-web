fcc5745a8460d503d994a523aa10e31051a491ad
Ntholi Nkhatho <ntholi@live.com>
2025-02-07 11:41:34 +0200

    Add logo to signup page

--- src/app/(main)/signup/page.tsx
+++ src/app/(main)/signup/page.tsx
@@ -4,6 +4,7 @@ import { SignupForm } from '@/app/(main)/signup/signup-form';
 import { getSignup } from '@/server/signups/actions';
 import { redirect } from 'next/navigation';
 import { Container } from '@/components/ui/container';
+import Logo from '../base/Logo';
 
 export default async function SignupPage() {
   const session = await auth();
@@ -18,14 +19,21 @@ export default async function SignupPage() {
   const existingSignup = await getSignup(session.user.id);
 
   return (
-    <Container className='flex items-center justify-center flex-col w-full h-screen'>
-      <div className='mb-8'>
-        <h1 className='text-3xl font-bold'>Sign Up</h1>
-        <p className='text-muted-foreground'>
-          Fill in your details to signup as a student
-        </p>
-      </div>
-      <SignupForm existingSignup={existingSignup} />
-    </Container>
+    <>
+      <Logo
+        width={300}
+        height={300}
+        className='absolute left-1/2 top-0 mt-4 h-20 sm:h-28 w-auto -translate-x-1/2 md:mt-10'
+      />
+      <Container className='flex items-center justify-center flex-col w-full h-screen gap-8'>
+        <div className='text-center'>
+          <h1 className='text-3xl sm:text-4xl font-semibold'>Sign Up</h1>
+          <p className='text-muted-foreground'>
+            Fill in your details to signup as a student
+          </p>
+        </div>
+        <SignupForm existingSignup={existingSignup} />
+      </Container>
+    </>
   );
 }
