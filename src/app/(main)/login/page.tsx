378a2b3c59d0a830248fc852045feca69117d037
Ntholi Nkhatho <ntholi@live.com>
2025-02-12 08:26:02 +0200

    add disclaimer message on login page

--- src/app/(main)/login/page.tsx
+++ src/app/(main)/login/page.tsx
@@ -6,10 +6,12 @@ import {
   CardDescription,
   CardHeader,
   CardTitle,
+  CardFooter,
 } from '@/components/ui/card';
 import { redirect } from 'next/navigation';
 import Logo from '../base/Logo';
 import GoogleIcon from './GoogleIcon';
+import { AlertCircle } from 'lucide-react';
 
 export const metadata = {
   title: 'Sign in',
@@ -49,6 +51,15 @@ export default async function LoginForm() {
             </Button>
           </form>
         </CardContent>
+        <CardFooter className='bg-yellow-100 text-yellow-800 p-4 mt-4 rounded-b-lg'>
+          <div className='flex items-start'>
+            <AlertCircle className='mr-2 h-5 w-5 flex-shrink-0' />
+            <p className='text-[0.8rem]'>
+              This app is currently in beta testing. If you encounter any
+              issues, please report them to the Registry Department.
+            </p>
+          </div>
+        </CardFooter>
       </Card>
     </div>
   );
