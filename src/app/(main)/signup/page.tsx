30e2ec4e039414bda3f2f1c4902a28ae41f5fc8c
Ntholi Nkhatho <ntholi@live.com>
2025-02-18 12:33:27 +0200

    fix issue with SignupPage logo

--- src/app/(main)/signup/page.tsx
+++ src/app/(main)/signup/page.tsx
@@ -22,15 +22,11 @@ export default async function SignupPage() {
 
   return (
     <>
-      <Logo
-        width={300}
-        height={300}
-        className='absolute left-1/2 top-0 mt-4 h-20 sm:h-28 w-auto -translate-x-1/2 md:mt-5'
-      />
-      <Container className='flex items-center justify-center flex-col w-full h-screen gap-8 md:pt-10'>
+      <Container className='flex h-screen w-full flex-col items-center justify-center gap-4'>
+        <Logo width={200} height={200} />
         <div className='text-center'>
-          <h1 className='text-3xl sm:text-4xl font-semibold'>Sign Up</h1>
-          <p className='text-muted-foreground'>
+          <h1 className='text-3xl font-semibold sm:text-4xl'>Sign Up</h1>
+          <p className='mt-1.5 text-muted-foreground'>
             Fill in your details to signup as a student
           </p>
         </div>
