e5f4dae5330977b4ad2ca546b95ef5a7af4a6d26
Ntholi Nkhatho <ntholi@live.com>
2025-02-19 14:02:35 +0200

    RoleDisplay component for admin panel

---
+++ src/app/admin/base/RoleDisplay.tsx
@@ -0,0 +1,18 @@
+'use client';
+import { Skeleton, Title } from '@mantine/core';
+import { useSession } from 'next-auth/react';
+import React from 'react';
+
+export default function RoleDisplay() {
+  const { data: session, status } = useSession();
+
+  if (status === 'loading') {
+    return <Skeleton height={40} width={170} mb={5} />;
+  }
+
+  return (
+    <Title fw={'lighter'} tt='capitalize'>
+      {session?.user?.role} Portal
+    </Title>
+  );
+}
--- src/app/admin/page.tsx
+++ src/app/admin/page.tsx
@@ -1,11 +1,12 @@
 import { Stack, Title } from '@mantine/core';
 import ActiveTermDisplay from './base/ActiveTermDisplay';
+import RoleDisplay from './base/RoleDisplay';
 
 export default function AdminPage() {
   return (
     <Stack h={'70vh'} w={'100%'} justify='center' align='center'>
       <div>
-        <Title fw={'lighter'}>Admin Panel</Title>
+        <RoleDisplay />
         <ActiveTermDisplay />
       </div>
     </Stack>
