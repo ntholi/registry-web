aaf958b455c6a31e857d077ced43cb392a8b2293
Ntholi Nkhatho <ntholi@live.com>
2025-02-23 20:40:31 +0200

    Add suppressHydrationWarning to body in RootLayout for improved SSR handling

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -24,7 +24,7 @@ export const metadata: Metadata = {
 export default function RootLayout({ children }: PropsWithChildren) {
   return (
     <html lang='en'>
-      <body>
+      <body suppressHydrationWarning>
         <Providers>{children}</Providers>
       </body>
     </html>
