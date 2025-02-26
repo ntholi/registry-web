09d81c615a1b4baf3958546d558d95f323bcfa0c
Ntholi Nkhatho <ntholi@live.com>
2025-02-26 08:57:32 +0200

    Enhance getFailedModules function to exclude deleted and deferred modules

--- src/app/(main)/(auth)/registration/remain/actions.ts
+++ src/app/(main)/(auth)/registration/remain/actions.ts
@@ -2,16 +2,21 @@
 
 import { db } from '@/db';
 import { studentPrograms } from '@/db/schema';
-import { eq } from 'drizzle-orm';
+import { and, eq, notInArray } from 'drizzle-orm';
 
 export async function getFailedModules(stdNo: number, semester: number) {
   const studentModules = await db.query.studentPrograms.findMany({
     where: eq(studentPrograms.stdNo, stdNo),
     with: {
       semesters: {
-        where: (semesters) => eq(semesters.semesterNumber, semester),
+        where: (s) =>
+          and(
+            eq(s.semesterNumber, semester),
+            notInArray(s.status, ['Deleted', 'Deferred']),
+          ),
         with: {
           studentModules: {
+            where: (m) => notInArray(m.status, ['Delete', 'Drop']),
             with: {
               module: true,
             },
@@ -21,10 +26,16 @@ export async function getFailedModules(stdNo: number, semester: number) {
     },
   });
 
-  const failedModules = studentModules
+  const allModules = studentModules
     .flatMap((prog) => prog.semesters)
-    .flatMap((sem) => sem.studentModules)
-    .filter((mod) => parseFloat(mod.marks) < 50)
+    .flatMap((sem) => sem.studentModules);
+
+  const passedModules = allModules
+    .filter((mod) => parseFloat(mod.marks) >= 50)
+    .map((mod) => mod.module.name);
+
+  const failedModules = allModules
+    .filter((mod) => !passedModules.includes(mod.module.name))
     .map((mod) => ({
       code: mod.module.code,
       name: mod.module.name,
