99599312f6a1c5df94f3f3f4323fd89895dda51b
Ntholi Nkhatho <ntholi@live.com>
2025-02-26 12:52:04 +0200

    Add role selection to UserForm and update user retrieval logic to include search by email and name

--- src/app/admin/users/Form.tsx
+++ src/app/admin/users/Form.tsx
@@ -1,33 +1,38 @@
 'use client';
 
-import { users } from '@/db/schema';
+import { users, userRoles } from '@/db/schema';
 import { Form } from '@/components/adease';
-import { TextInput } from '@mantine/core';
+import { TextInput, Select } from '@mantine/core';
 import { createInsertSchema } from 'drizzle-zod';
 import { useRouter } from 'next/navigation';
+import { z } from 'zod';
 
 type User = typeof users.$inferInsert;
 
-
 type Props = {
-  onSubmit: (values: User) => Promise<User>;
-  defaultValues?: User;
+  onSubmit: (values: Pick<User, 'name' | 'role'>) => Promise<User>;
+  defaultValues?: Partial<User>;
   onSuccess?: (value: User) => void;
   onError?: (
-    error: Error | React.SyntheticEvent<HTMLDivElement, Event>
+    error: Error | React.SyntheticEvent<HTMLDivElement, Event>,
   ) => void;
   title?: string;
 };
 
 export default function UserForm({ onSubmit, defaultValues, title }: Props) {
   const router = useRouter();
-  
+
+  const userFormSchema = z.object({
+    name: z.string().min(1, 'Name is required'),
+    role: z.enum(userRoles),
+  });
+
   return (
-    <Form 
+    <Form
       title={title}
-      action={onSubmit} 
+      action={onSubmit}
       queryKey={['users']}
-      schema={createInsertSchema(users)} 
+      schema={userFormSchema}
       defaultValues={defaultValues}
       onSuccess={({ id }) => {
         router.push(`/admin/users/${id}`);
@@ -36,8 +41,14 @@ export default function UserForm({ onSubmit, defaultValues, title }: Props) {
       {(form) => (
         <>
           <TextInput label='Name' {...form.getInputProps('name')} />
-          <TextInput label='Email' {...form.getInputProps('email')} />
-          <TextInput label='Image' {...form.getInputProps('image')} />
+          <Select
+            label='Role'
+            data={userRoles.map((role) => ({
+              value: role,
+              label: role.charAt(0).toUpperCase() + role.slice(1),
+            }))}
+            {...form.getInputProps('role')}
+          />
         </>
       )}
     </Form>
--- src/server/users/actions.ts
+++ src/server/users/actions.ts
@@ -1,18 +1,16 @@
 'use server';
 
-
 import { users } from '@/db/schema';
-import { usersService as service} from './service';
+import { usersService as service } from './service';
 
 type User = typeof users.$inferInsert;
-
 
 export async function getUser(id: string) {
   return service.get(id);
 }
 
 export async function findAllUsers(page: number = 1, search = '') {
-  return service.findAll({ page, search });
+  return service.findAll({ page, search, searchProperties: ['email', 'name'] });
 }
 
 export async function createUser(user: User) {
