0f36f4e2159c78e0245150914d4247b0683e0f51
Ntholi Nkhatho <ntholi@live.com>
2025-04-18 10:29:19 +0200

    Add lecturer details page with tabs and module assignment button

--- src/app/admin/lecturers/[id]/page.tsx
+++ src/app/admin/lecturers/[id]/page.tsx
@@ -1,11 +1,21 @@
-import {
-  DetailsView,
-  DetailsViewBody,
-  DetailsViewHeader,
-  FieldView,
-} from '@/components/adease';
+import { DetailsView, DetailsViewHeader } from '@/components/adease';
 import { notFound } from 'next/navigation';
 import { getUser } from '@/server/users/actions';
+import {
+  Box,
+  Divider,
+  Group,
+  Stack,
+  Tabs,
+  TabsList,
+  TabsPanel,
+  TabsTab,
+  Title,
+  Text,
+  Flex,
+  Button,
+} from '@mantine/core';
+import { IconPlus } from '@tabler/icons-react';
 
 type Props = {
   params: Promise<{ id: string }>;
@@ -20,11 +30,36 @@ export default async function UserDetails({ params }: Props) {
   }
 
   return (
-    <DetailsView>
-      <DetailsViewHeader title={'Lecturer'} queryKey={['lecturers']} />
-      <DetailsViewBody>
-        <FieldView label='Name'>{lecturer.name}</FieldView>
-      </DetailsViewBody>
-    </DetailsView>
+    <Stack p={'xl'}>
+      <Flex justify={'space-between'} align={'end'}>
+        <Box>
+          <Title order={2} fw={100}>
+            {lecturer.name}
+          </Title>
+          <Text c='dimmed' size='sm'>
+            Assigned Modules
+          </Text>
+        </Box>
+        <Button size='sm' variant='light' leftSection={<IconPlus size={14} />}>
+          Assign Module
+        </Button>
+      </Flex>
+
+      <Divider />
+      <Box px='lg' py='md'>
+        <Tabs defaultValue='gallery' orientation='vertical'>
+          <TabsList>
+            <TabsTab value='gallery'>Gallery</TabsTab>
+            <TabsTab value='messages'>Messages</TabsTab>
+            <TabsTab value='settings'>Settings</TabsTab>
+          </TabsList>
+          <Box p='lg'>
+            <TabsPanel value='gallery'>Gallery tab content</TabsPanel>
+            <TabsPanel value='messages'>Messages tab content</TabsPanel>
+            <TabsPanel value='settings'>Settings tab content</TabsPanel>
+          </Box>
+        </Tabs>
+      </Box>
+    </Stack>
   );
 }
