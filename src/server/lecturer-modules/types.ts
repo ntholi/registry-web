2fd6ea7db31d7f462d3ba13cf1e35974f30afd19
Ntholi Nkhatho <ntholi@live.com>
2025-04-18 05:42:05 +0200

    Looks like no actual code changes were provided to analyze. I'll need to see the diff/changes to write an appropriate commit message.

--- src/server/lecturer-modules/types.ts
+++
@@ -1,7 +0,0 @@
-import { z } from 'zod';
-
-export const lecturesModule = z.object({
-  id: z.number().optional(),
-  moduleId: z.number(),
-});
-export type LecturesModule = z.infer<typeof lecturesModule>;
