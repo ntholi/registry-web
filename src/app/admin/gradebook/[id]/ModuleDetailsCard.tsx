4629b487cdc7e75ac2d6904dea8a252881512ebf
Ntholi Nkhatho <ntholi@live.com>
2025-05-19 19:24:42 +0200

    feat: replace Card with Paper in ModuleDetailsCard and remove unused imports

--- src/app/admin/gradebook/[id]/ModuleDetailsCard.tsx
+++ src/app/admin/gradebook/[id]/ModuleDetailsCard.tsx
@@ -1,8 +1,5 @@
-import {
-  getAssignedModule,
-  getAssignedModuleByUserAndModule,
-} from '@/server/assigned-modules/actions';
-import { Card, Group, Stack, Text, Title } from '@mantine/core';
+import { getAssignedModuleByUserAndModule } from '@/server/assigned-modules/actions';
+import { Paper, Group, Stack, Text, Title } from '@mantine/core';
 import { IconBook, IconCalendar, IconUserCheck } from '@tabler/icons-react';
 
 type ModuleDetailsCardProps = {
@@ -17,12 +14,11 @@ export default function ModuleDetailsCard({
   studentsCount,
 }: ModuleDetailsCardProps) {
   return (
-    <Card withBorder radius='md' shadow='sm' p='lg' mb='lg'>
+    <Paper withBorder p='lg' mb='lg'>
       <Group justify='space-between' wrap='nowrap'>
         <Stack gap='xs'>
           <Group gap='xs'>
-            <IconBook size={20} stroke={1.5} />
-            <Title order={2} fw={600}>
+            <Title order={2} fw={100}>
               {module.semesterModule.module!.name}
             </Title>
           </Group>
@@ -48,6 +44,6 @@ export default function ModuleDetailsCard({
           </Group>
         </Stack>
       </Group>
-    </Card>
+    </Paper>
   );
 }
