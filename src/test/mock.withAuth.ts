32b2b382679e6cd34316dab93b0766be59342b56
Ntholi Nkhatho <ntholi@live.com>
2025-07-01 15:16:47 +0200

    feat: enhance mockWithAuth function with access check and role handling

--- src/test/mock.withAuth.ts
+++ src/test/mock.withAuth.ts
@@ -1,31 +1,72 @@
 import { vi } from 'vitest';
 import { getMockUser } from '@/test/mocks.auth';
+import { dashboardUsers, UserRole } from '@/db/schema';
+import { Session } from 'next-auth';
+
+type Role = UserRole | 'all' | 'auth' | 'dashboard';
+
+// Mock next/navigation functions
+vi.mock('next/navigation', () => ({
+  forbidden: vi.fn(() => {
+    throw new Error('Forbidden');
+  }),
+  unauthorized: vi.fn(() => {
+    throw new Error('Unauthorized');
+  }),
+}));
+
+import { forbidden, unauthorized } from 'next/navigation';
 
 export const mockWithAuth = vi.fn(
-  async (fn: Function, roles: string[] = []) => {
-    const user = getMockUser();
+  async <T>(
+    fn: (session?: Session | null) => Promise<T>,
+    roles: Role[] = [],
+    accessCheck?: (session: Session) => Promise<boolean>,
+  ) => {
+    const session = {
+      user: getMockUser(),
+      expires: new Date(Date.now() + 24 * 60 * 60 * 1000).toISOString(),
+    } as Session;
 
-    if (roles.includes('all')) {
-      return fn();
+    const callFnWithAccessCheck = async (session?: Session | null) => {
+      if (accessCheck && session?.user) {
+        const isAuthorized = await accessCheck(session);
+        if (!isAuthorized && session.user.role !== 'admin') {
+          return forbidden();
+        }
+      }
+      return fn(session);
+    };
+
+    if (roles.length === 1 && roles.includes('all')) {
+      return callFnWithAccessCheck(session);
     }
 
-    if (!user) {
-      throw new Error('Unauthorized');
+    if (!session?.user) {
+      return unauthorized();
     }
 
-    if (roles.length === 0 && user?.role === 'admin') {
-      return fn();
+    if (roles.includes('auth') && session?.user) {
+      return callFnWithAccessCheck(session);
     }
 
-    if (user?.role === 'admin') {
-      return fn();
+    if (
+      roles.includes('dashboard') &&
+      dashboardUsers.includes(
+        session?.user?.role as (typeof dashboardUsers)[number],
+      )
+    ) {
+      return callFnWithAccessCheck(session);
     }
 
-    if (user?.role && roles.includes(user.role)) {
-      return fn();
+    if (!['admin', ...roles].includes(session.user.role as Role)) {
+      if (roles.length === 0) {
+        return unauthorized();
+      }
+      return forbidden();
     }
 
-    throw new Error('Unauthorized');
+    return callFnWithAccessCheck(session);
   },
 );
 
