727d87bf59c98c56149c68a1bcab569976ad72aa
Ntholi Nkhatho <ntholi@live.com>
2025-09-04 14:34:22 +0200

    fix(academics): set default isActive prop to false in AcademicsView

--- src/app/dashboard/students/[id]/AcademicsView/index.tsx
+++ src/app/dashboard/students/[id]/AcademicsView/index.tsx
@@ -35,7 +35,7 @@ type Props = {
 export default function AcademicsView({
   stdNo,
   showMarks,
-  isActive = true,
+  isActive = false,
   ...props
 }: Props) {
   const { data: student, isLoading } = useQuery({
