19f8154f7841ee9272ba0d08f4fbe504db0e73fd
Ntholi Nkhatho <ntholi@live.com>
2025-10-10 14:47:24 +0200

    init ExportButton

--- src/app/dashboard/gradebook/[id]/ModuleDetailsCard.tsx
+++ src/app/dashboard/gradebook/[id]/ModuleDetailsCard.tsx
@@ -3,10 +3,20 @@
 import { useCurrentTerm } from '@/hooks/use-current-term';
 import { toClassName } from '@/lib/utils';
 import { getAssignedModuleByUserAndModule } from '@/server/assigned-modules/actions';
-import { Group, Paper, Select, Stack, Text, Title } from '@mantine/core';
+import {
+  Box,
+  Flex,
+  Group,
+  Paper,
+  Select,
+  Stack,
+  Text,
+  Title,
+} from '@mantine/core';
 import { IconBook, IconCalendar, IconChevronDown } from '@tabler/icons-react';
 import { useQueryState } from 'nuqs';
 import { useEffect, useMemo } from 'react';
+import ExportButton from './export/ExportButton';
 
 type ModuleDetailsCardProps = {
   modules: NonNullable<
@@ -30,7 +40,7 @@ export default function ModuleDetailsCard({ modules }: ModuleDetailsCardProps) {
           value: program.id.toString(),
           label: toClassName(
             module.semesterModule.semester?.structure.program.code || '',
-            module.semesterModule.semester?.name || '',
+            module.semesterModule.semester?.name || ''
           ),
         });
       }
@@ -51,28 +61,17 @@ export default function ModuleDetailsCard({ modules }: ModuleDetailsCardProps) {
     }
     return modules.find(
       (it) =>
-        it.semesterModule?.semester?.structure.program.id === Number(programId),
+        it.semesterModule?.semester?.structure.program.id === Number(programId)
     )?.semesterModule?.semester?.structure.program;
   }, [modules, programId]);
 
   return (
     <Paper withBorder p='lg' mb='lg'>
-      <Group justify='space-between' wrap='nowrap'>
-        <Stack gap='xs' style={{ flex: 1 }}>
-          <Group gap='xs' align='center' justify='space-between'>
-            <Title order={2} fw={100}>
-              {modules.at(0)?.semesterModule?.module?.name}
-            </Title>
-
-            <Select
-              placeholder='Select Program'
-              data={moduleOptions}
-              value={programId || ''}
-              onChange={(value) => setProgramId(value === '' ? null : value)}
-              rightSection={<IconChevronDown size={16} />}
-              clearable
-            />
-          </Group>{' '}
+      <Flex justify='space-between' wrap='nowrap'>
+        <Stack gap={'xs'}>
+          <Title order={2} fw={100}>
+            {modules.at(0)?.semesterModule?.module?.name}
+          </Title>
           <Group gap='md' align='center'>
             <Paper bg='var(--mantine-color-default)' px='sm' py={3} withBorder>
               <Text ff='monospace' fw={600} size='sm'>
@@ -94,7 +93,18 @@ export default function ModuleDetailsCard({ modules }: ModuleDetailsCardProps) {
             </Group>
           </Group>
         </Stack>
-      </Group>
+        <Stack gap={'xs'}>
+          <Select
+            placeholder='Select Program'
+            data={moduleOptions}
+            value={programId || ''}
+            onChange={(value) => setProgramId(value === '' ? null : value)}
+            rightSection={<IconChevronDown size={16} />}
+            clearable
+          />
+          <ExportButton />
+        </Stack>
+      </Flex>
     </Paper>
   );
 }
---
+++ src/app/dashboard/gradebook/[id]/export/ExportButton.tsx
@@ -0,0 +1,5 @@
+import React from 'react';
+
+export default function ExportButton() {
+  return <div>ExportButton</div>;
+}
