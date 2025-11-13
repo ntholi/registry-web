25d8ca3c9919b13061147e63f3449507d20b535c
Ntholi Nkhatho <ntholi@live.com>
2025-11-13 09:35:07 +0200

    feat(CoursesLoading): Refactor loading component to use Card and CardSection for improved layout

--- src/app/(classroom)/courses/loading.tsx
+++ src/app/(classroom)/courses/loading.tsx
@@ -1,15 +1,67 @@
-import { Container, SimpleGrid, Skeleton, Stack } from '@mantine/core';
+import {
+	Box,
+	Card,
+	CardSection,
+	Container,
+	Flex,
+	SimpleGrid,
+	Skeleton,
+	Stack,
+} from '@mantine/core';
 
 export default function CoursesLoading() {
 	return (
 		<Container mt='lg' size='xl'>
-			<SimpleGrid cols={{ base: 1, sm: 2, md: 3, xl: 4 }}>
+			<SimpleGrid cols={{ base: 1, sm: 2, md: 3 }}>
 				{[1, 2, 3, 4, 5, 6].map((i) => (
-					<Stack key={i} gap='sm'>
-						<Skeleton height={120} radius='md' />
-						<Skeleton height={24} width='80%' radius='sm' />
-						<Skeleton height={16} width='60%' radius='sm' />
-					</Stack>
+					<Card
+						key={i}
+						shadow='lg'
+						padding='lg'
+						radius='sm'
+						withBorder
+						style={{ overflow: 'hidden' }}
+					>
+						<CardSection
+							style={{
+								height: '8.5rem',
+								position: 'relative',
+								overflow: 'hidden',
+							}}
+						>
+							<Skeleton height='100%' radius={6} />
+
+							<Box
+								style={{
+									position: 'absolute',
+									zIndex: 2,
+									inset: 0,
+									display: 'flex',
+									alignItems: 'center',
+									justifyContent: 'space-between',
+									padding: '0 2rem',
+								}}
+							>
+								<Box
+									style={{
+										display: 'flex',
+										alignItems: 'center',
+										justifyContent: 'center',
+									}}
+								>
+									<Skeleton height={72} width={72} radius='xl' />
+								</Box>
+								<Box />
+							</Box>
+						</CardSection>
+
+						<Stack gap='sm' mt='md'>
+							<Flex justify={'space-between'} align={'baseline'}>
+								<Skeleton height={22} width='65%' radius='sm' />
+								<Skeleton height={20} width={80} radius='sm' />
+							</Flex>
+						</Stack>
+					</Card>
 				))}
 			</SimpleGrid>
 		</Container>
