ca8d58a981a962524712d95dca7d675baca8be35
Ntholi Nkhatho <ntholi@live.com>
2025-11-17 14:28:17 +0200

    Refactor code structure for improved readability and maintainability

---
+++ drizzle/0028_silly_mole_man.sql
@@ -0,0 +1,2 @@
+ALTER TABLE "lecturer_allocations" ALTER COLUMN "number_of_students" SET DEFAULT 0;--> statement-breakpoint
+ALTER TABLE "lecturer_allocations" ALTER COLUMN "number_of_students" SET NOT NULL;
