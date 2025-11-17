df947d8be5ac9fcbf734e078772092d0b5d444d9
Ntholi Nkhatho <ntholi@live.com>
2025-11-17 06:00:28 +0200

    fix(venueTypes-import): correct import path for venueTypes in VenueTypeForm component

--- src/modules/timetable/features/venue-types/components/Form.tsx
+++ src/modules/timetable/features/venue-types/components/Form.tsx
@@ -3,7 +3,7 @@
 import { Textarea, TextInput } from '@mantine/core';
 import { createInsertSchema } from 'drizzle-zod';
 import { useRouter } from 'nextjs-toploader/app';
-import { venueTypes } from '@/core/database';
+import { venueTypes } from '@/modules/timetable/database';
 import { Form } from '@/shared/ui/adease';
 
 type VenueType = typeof venueTypes.$inferInsert;
