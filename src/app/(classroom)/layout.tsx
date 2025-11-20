9b584ac58d3cac28647cbef51c11136e9f452391
Ntholi Nkhatho <ntholi@live.com>
2025-11-20 10:42:50 +0200

    refactor(layout): simplify layout by re-exporting from dashboard layout

--- src/app/(classroom)/layout.tsx
+++ src/app/(classroom)/layout.tsx
@@ -1,27 +1,4 @@
-import { Alert } from '@mantine/core';
-import type { Metadata } from 'next';
-import type { PropsWithChildren } from 'react';
-import { DebugRibbon } from '@/shared/ui/DebugRibbon';
-import Dashboard from '../dashboard/dashboard';
-
-export async function generateMetadata(): Promise<Metadata> {
-	return {
-		title: `Classroom | Limkokwing`,
-	};
-}
-
-export default function AdminLayout({ children }: PropsWithChildren) {
-	const isLocal = process.env.DATABASE_ENV === 'local';
-	return (
-		<Dashboard>
-			{!process.env.AUTH_URL?.includes('portal.co.ls') && (
-				<DebugRibbon isLocal={isLocal} />
-			)}
-			<Alert title='Notice' color='yellow' m='lg' mt={'sm'}>
-				Google Classroom integration is currently under development. Some
-				features may not function as expected.
-			</Alert>
-			{children}
-		</Dashboard>
-	);
-}
+export {
+	default,
+	generateMetadata,
+} from '../dashboard/layout';
