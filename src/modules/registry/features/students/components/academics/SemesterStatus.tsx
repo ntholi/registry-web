20203f8d61ea2be1238cb0adfc0c03d212d9aa18
Ntholi Nkhatho <ntholi@live.com>
2025-12-19 11:39:30 +0200

    feat: move SemesterStatus component to academics directory and enhance color logic for status representation

---
+++ src/modules/registry/features/students/components/academics/SemesterStatus.tsx
@@ -0,0 +1,39 @@
+import { Badge } from '@mantine/core';
+import type { semesterStatus } from '@/core/database';
+
+type SemesterStatus = (typeof semesterStatus.enumValues)[number];
+
+type Props = {
+	status: SemesterStatus;
+};
+
+export default function SemesterStatusView({ status }: Props) {
+	const getColor = (status: SemesterStatus) => {
+		switch (status) {
+			case 'Active':
+			case 'Enrolled':
+				return 'gray';
+			case 'Outstanding':
+				return 'dark';
+			case 'Deleted':
+			case 'Inactive':
+			case 'DNR':
+			case 'DroppedOut':
+				return 'red';
+			case 'Deferred':
+				return 'yellow';
+			case 'Exempted':
+				return 'blue';
+			case 'Repeat':
+				return 'violet';
+			default:
+				return 'gray';
+		}
+	};
+
+	return (
+		<Badge color={getColor(status)} size='sm'>
+			{status}
+		</Badge>
+	);
+}
