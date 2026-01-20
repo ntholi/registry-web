242db3737d63c776a482ff9635d805fa15c99539
Ntholi Nkhatho <ntholi@live.com>
2026-01-20 10:22:20 +0200

    feat: allow null values for date_of_birth, nationality, and gender in applicants table

---
+++ drizzle/0091_wise_venus.sql
@@ -0,0 +1,3 @@
+ALTER TABLE "applicants" ALTER COLUMN "date_of_birth" DROP NOT NULL;--> statement-breakpoint
+ALTER TABLE "applicants" ALTER COLUMN "nationality" DROP NOT NULL;--> statement-breakpoint
+ALTER TABLE "applicants" ALTER COLUMN "gender" DROP NOT NULL;
