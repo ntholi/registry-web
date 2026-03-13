6f9d3f84643950df5dcaa8703271590ed9486da6
Ntholi Nkhatho <ntholi@live.com>
2026-03-13 20:22:15 +0200

    feat: remove 'better-auth' from serverExternalPackages in next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -2,7 +2,6 @@ import type { NextConfig } from 'next';
 
 const nextConfig: NextConfig = {
 	reactCompiler: true,
-	serverExternalPackages: ['better-auth'],
 	experimental: {
 		optimizePackageImports: ['@mantine/core', '@mantine/hooks'],
 		authInterrupts: true,
