44fcfc60196083e3b03a3351186fd85a0a326e21
Ntholi Nkhatho <ntholi@live.com>
2026-03-25 11:03:54 +0200

    feat: add restrictions to letter templates for various student statuses

--- drizzle/0204_clammy_ben_grimm.sql
+++ drizzle/0204_clammy_ben_grimm.sql
@@ -1,1 +1,19 @@
-ALTER TABLE "letter_templates" ADD COLUMN "restrictions" jsonb DEFAULT '[]'::jsonb;
+ALTER TABLE "letter_templates" ADD COLUMN "restrictions" jsonb DEFAULT '[]'::jsonb;--> statement-breakpoint
+
+UPDATE "letter_templates" SET "restrictions" = '[{"type":"semesterStatus","operator":"include","values":["Active","Enrolled","Exempted","Outstanding","Repeat","DNR"]}]'::jsonb
+WHERE "id" IN ('tpl_confirmation_student', 'tpl_industry_request');--> statement-breakpoint
+
+UPDATE "letter_templates" SET "restrictions" = '[{"type":"studentStatus","operator":"include","values":["Active"]},{"type":"semesterStatus","operator":"include","values":["Active","Enrolled","Exempted","Outstanding","Repeat","DNR"]}]'::jsonb
+WHERE "id" IN ('tpl_identity_card', 'tpl_passport', 'tpl_visa');--> statement-breakpoint
+
+UPDATE "letter_templates" SET "restrictions" = '[{"type":"studentStatus","operator":"include","values":["Active"]},{"type":"programStatus","operator":"include","values":["Active"]},{"type":"semesterStatus","operator":"include","values":["Active","Enrolled","Exempted","Outstanding","Repeat","DNR"]}]'::jsonb
+WHERE "id" = 'tpl_change_programme';--> statement-breakpoint
+
+UPDATE "letter_templates" SET "restrictions" = '[{"type":"programStatus","operator":"include","values":["Completed"]}]'::jsonb
+WHERE "id" IN ('tpl_completion_qual', 'tpl_good_conduct', 'tpl_reference');--> statement-breakpoint
+
+UPDATE "letter_templates" SET "restrictions" = '[{"type":"studentStatus","operator":"include","values":["Active"]},{"type":"semesterStatus","operator":"include","values":["Deferred"]}]'::jsonb
+WHERE "id" = 'tpl_deferment';--> statement-breakpoint
+
+UPDATE "letter_templates" SET "restrictions" = '[{"type":"studentStatus","operator":"include","values":["Active"]},{"type":"semesterStatus","operator":"include","values":["Withdrawn"]}]'::jsonb
+WHERE "id" = 'tpl_withdrawal';
